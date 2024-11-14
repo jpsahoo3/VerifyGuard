8b880b450b4c34e4352582c705eac6793166229a
Vikrant Yadav <vikrantmani45@gmail.com>
2024-11-14 13:41:59 +0530

    initial home page

--- VerifyGuard/src/App.jsx
+++ VerifyGuard/src/App.jsx
@@ -1,17 +1,19 @@
-import { useState } from 'react'
-import { BrowserRouter as Router } from 'react-router-dom'
-import './App.css'
-import Navbar from './components/Navbar'
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
+import Navbar from './components/Navbar';
+import Home from './pages/Home'; 
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <Router>
-      <Navbar/>
+      <Navbar />
+      <div className="container mx-auto px-4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
