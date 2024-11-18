ee3ffae5299e005089d8ffe8d1d824441086793d
Vikrant Yadav <vikrantmani45@gmail.com>
2024-11-18 09:13:30 +0530

    Search bar testing with virus total api key

--- VerifyGuard/src/pages/Home.jsx
+++ VerifyGuard/src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-// import SearchBar from '../components/SearchBar';
+import SearchBar from '../components/SearchBar';
 
 const Home = () => {
   return (
@@ -7,7 +7,7 @@ const Home = () => {
       <h1 className="text-3xl font-bold text-center mb-4">Check if a company is Verified</h1>
       <p className="text-center text-gray-600 mb-8">Powered by Psylinks</p>
       <div className="flex justify-center">
-        {/* <SearchBar /> */}
+        <SearchBar />
       </div>
     </div>
   );
